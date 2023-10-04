2dad4b8af215637cdd7e375bc582bbae6307e9eb
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-10-04 02:27:02 -0500

    Update view-classes.php

--- view-classes.php
+++ view-classes.php
@@ -15,7 +15,7 @@ while ($class=$classes -> fetch_assoc()) {
 ?>
   <tr>
     <td><?php echo $class['class_id'];?></td>
-    <td><?php echo $class['class_name'];?></td>
+    <td><?php echo $class['class_number'];?></td>
     <td><?php echo $class['class_description'];?></td>
   </tr>
   <?php
