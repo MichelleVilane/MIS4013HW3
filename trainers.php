658777ca850c2ada30130bea9fc155a828c544bf
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-10-03 23:57:09 -0500

    Update trainers.php

--- trainers.php
+++ trainers.php
@@ -1,1 +1,10 @@
-
+<?php
+require_once("util-db.php");
+require_once("model-trainers.php")
+  
+$pageTitle= "Home";
+include "view-header.php";
+$trainers = selectTrainers();
+include "view-trainers.php";
+include "view-footer.php";
+?>
