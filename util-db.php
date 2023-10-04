ab59c23c68d718ca494649f901da3ed5a72d8875
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-10-04 01:28:40 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('159.89.47.44', ''vilaneou_hw3user, 'F3uadDN.mM@Q', 'vilaneou_hw3');
+    $conn = new mysqli('159.89.47.44', 'vilaneou_hw3user', 'F3uadDN.mM@Q', 'vilaneou_hw3');
     
     // Check connection
     if ($conn->connect_error) {
