18ed9627112e56a48580692af6c1540acf0ed5ed
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-10-04 02:42:26 -0500

    Update classes-by-trainer.php

--- classes-by-trainer.php
+++ classes-by-trainer.php
@@ -1,1 +1,10 @@
-
+<?php
+require_once("util-db.php");
+require_once("model-classes-by-trainer.php");
+  
+$pageTitle= "Classes by Trainer";
+include "view-header.php";
+$trainers = selectClassesByTrainer();
+include "view-classes-by-trainer.php";
+include "view-footer.php";
+?>
