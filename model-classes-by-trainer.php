df07e0490e0fa3bfeb6ca15cfc0d7d1965c238e0
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-10-04 03:01:54 -0500

    Create model-classes-by-trainer.php

---
+++ model-classes-by-trainer.php
@@ -0,0 +1,16 @@
+<?php
+function selectClassesByTrainer($tid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT c.class_id, class_number, class_descripion, daytime FROM `class`c join session s on s.class_id=c.class_id where s.trainer_id=?");
+        $stmt->bind_param("t", $tid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
