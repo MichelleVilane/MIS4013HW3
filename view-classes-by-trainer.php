db3971e91c9c3d56496ee5b8d14131dc6713a67e
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-10-04 03:05:45 -0500

    Create view-classes-by-trainer.php

---
+++ view-classes-by-trainer.php
@@ -0,0 +1,30 @@
+<h1>Classes by Trainer</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+       <th>Number</th>
+       <th>Description</th>
+        <th>Daytime</th>
+       
+      </tr>
+      
+    </thead>
+      <tbody>
+        <?php
+while ($class=$classes -> fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $class['class_id'];?></td>
+    <td><?php echo $class['class_number'];?></td>
+    <td><?php echo $class['class_description'];?></td>
+     <td><?php echo $class['daytime'];?></td>
+  </tr>
+  <?php
+  
+}
+       ?>
+      </tbody>
+  </table>
+</div>
