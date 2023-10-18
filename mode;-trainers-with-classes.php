85e9ed05afd7772d945923d9c95c236d3cefd969
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-10-18 14:00:24 -0500

    Create mode;-trainers-with-classes.php

---
+++ mode;-trainers-with-classes.php
@@ -0,0 +1,15 @@
+<?php
+function selectTrainers() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT trainer_id, trainer_name, studio_number FROM `trainer`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
