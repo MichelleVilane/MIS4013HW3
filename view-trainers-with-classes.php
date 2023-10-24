83f8fdadb7310d655384ebcfa772bff94bdbca16
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-10-24 11:16:27 -0500

    Update and rename view-trainers-with-classes to view-trainers-with-classes.php

--- view-trainers-with-classes.php
+++ view-trainers-with-classes.php
@@ -9,13 +9,17 @@ while ($trainer=$trainers -> fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $trainer['trainer_name'];?></h5>
       <p class="card-text">
+      <ul class="list-group">        
   <?php
         $sessions = selectClassesByTrainer($trainer['trainer_id']);
         while ($class = $classes ->fetch_assoc()) {
       ?>
+    <li class="list-group-item"><?php echo $class['class_number'];?> - <?php echo $class['daytime'];?> - <?php echo $class['class_number'];?></li>
+              
           <?php
         }
    >?
+           </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary">Office: <?php echo $trainer['studio_number'];?></small></p>
     </div>
