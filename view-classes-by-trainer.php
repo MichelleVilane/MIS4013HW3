3299d68c59091a07ae0f584da38ba58f2bfd1705
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-10-25 01:28:25 -0500

    Update view-classes-by-trainer.php

--- view-classes-by-trainer.php
+++ view-classes-by-trainer.php
@@ -16,13 +16,12 @@
 while ($class=$classes -> fetch_assoc()) {
 ?>
   <tr>
-    <td><?php echo $class['class_id'];?></td>
-    <td><?php echo $class['class_number'];?></td>
-    <td><?php echo $class['class_description'];?></td>
-     <td><?php echo $class['daytime'];?></td>
+    <td><?php echo $class['class_id']; ?></td>
+    <td><?php echo $class['class_number']; ?></td>
+    <td><?php echo $class['class_description']; ?></td>
+     <td><?php echo $class['daytime']; ?></td>
   </tr>
   <?php
-  
 }
        ?>
       </tbody>
