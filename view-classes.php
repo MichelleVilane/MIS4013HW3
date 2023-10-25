8ffdd66503acdd9b7b40a743c5a421d1d0e57359
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-10-25 04:09:16 -0500

    Update view-classes.php

--- view-classes.php
+++ view-classes.php
@@ -16,7 +16,9 @@ include "view-classes-newform.php";
        <th>Number</th>
        <th>Description</th>
         <th>Session</th>
-      <th></th>
+          <th></th>
+          <th></th>
+          <th></th>
       </tr>
     </thead>
       <tbody>
@@ -27,8 +29,19 @@ while ($class=  $classes -> fetch_assoc()) {
     <td><?php echo $class['class_id'];?></td>
     <td><?php echo $class['class_number'];?></td>
     <td><?php echo $class['class_description'];?></td>
-   
+   <td>
+   </td>
     <td>
+        <form method="post" action="session-by-class.php">
+        <input type="hidden" name="cid" value="<?php echo $class['class_id'];?>">
+         <input type="hidden" name="actionType" value="Delete">   
+        <button type="submit" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+          <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+        </svg>
+                </button>
+          </form>
+    </td>
+      <td>
       <form method="post" action="session-by-class.php">
         <input type="hidden" name="cid" value="<?php echo $class['class_id'];?>">
         <button type="submit" class="btn btn-primary">Session</button>
