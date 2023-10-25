afff55d8a5aacd51ec0526e7c92dff95d96b605c
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-10-25 02:40:14 -0500

    Update view-classes-newform.php

--- view-classes-newform.php
+++ view-classes-newform.php
@@ -10,10 +10,10 @@
 <div class="modal fade" id="newClassModal" tabindex="-1" aria-labelledby="newClassModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-      <div class="modal-header">
-  <h1 class="h1" id="newClassModalLabel">New Class</h1>
-  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-</div>
+     <div class="modal-header">
+        <h1 class="modal-title fs-5" id="newClassModalLabel">New Class</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
       <div class="modal-body">
       <form method="post" action="">
           <div class="mb-3">
