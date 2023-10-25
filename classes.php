be6b235a77b9878da1a3b58d0d0ac5b5d356615e
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-10-25 03:20:46 -0500

    Update classes.php

--- classes.php
+++ classes.php
@@ -4,6 +4,13 @@ require_once("model-classes.php");
   
 $pageTitle= "Classes";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+ switch($_POST ['actionType'])){
+   case "Add":
+     insertClass($_POST['cNumber'], $_POST['cDescription']);
+   break
+ }
 $classes = selectClasses();
 include "view-classes.php";
 include "view-footer.php";
