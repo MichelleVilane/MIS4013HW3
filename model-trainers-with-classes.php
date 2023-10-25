3736d56161f04a4c9971c1cfd9320c51b686e9cc
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-10-24 23:57:20 -0500

    Update model-trainers-with-classes.php

--- model-trainers-with-classes.php
+++ model-trainers-with-classes.php
@@ -17,7 +17,7 @@ function selectTrainers() {
 function selectClassesByTrainer($tid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT c.class_id, class_number, class_descripion, daytime FROM `class`c join session s on s.class_id=c.class_id where s.trainer_id=?");
+        $stmt = $conn->prepare("SELECT c.class_id, class_number, class_description, daytime FROM `class`c join session s on s.class_id=c.class_id where s.trainer_id=?");
         $stmt->bind_param("i", $tid);
         $stmt->execute();
         $result = $stmt->get_result();
