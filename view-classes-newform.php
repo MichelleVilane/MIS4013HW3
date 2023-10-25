abb345dee852a6588587af2538dfa0b7a8047942
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-10-25 03:17:33 -0500

    Update view-classes-newform.php

--- view-classes-newform.php
+++ view-classes-newform.php
@@ -24,7 +24,7 @@
             <label for="cDescription" class="form-label">Class Description</label>
             <input type="text" class="form-control" id="cDescription" name="cDescription">
           </div>
-        <input type="hidden" name="actionType" value="Add"
+        <input type="hidden" name="actionType" value="Add">
           <button type="submit" class="btn btn-primary">Save</button>
       </form>
       </div>
