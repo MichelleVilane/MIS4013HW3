50e44ddf2547c62c1da8e28af4bed896569505ab
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-10-25 11:45:57 -0500

    Update model-trainers-with-classes.php

--- model-trainers-with-classes.php
+++ model-trainers-with-classes.php
@@ -28,5 +28,72 @@ function selectClassesByTrainer($tid) {
         throw $e;
     }
 }
+
+function selectTrainersForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT trainer_id, trainer_name FROM `trainer` order by trainer_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function selectClassessForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT class_id, class_number, class_description FROM `class` order by class_description");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function insertSession($cNumber, $cDescription) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `class` ( `class_number`, `class_description`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $cNumber, $cDescription);
+        $success= $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateClass($cNumber, $cDescription, $cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `class` set `class_number`= ?, `class_description`= ? where class_id=?");
+        $stmt->bind_param("ssi", $cNumber, $cDescription, $cid);
+        $success= $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteClass($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from class where class_id=?");       
+        $stmt->bind_param("i", $cid);
+        $success= $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
 ?>
 
