1838d04049ec62e27c76dc55292e6593c157f99e
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-11-14 20:35:09 -0600

    Create trainers-chart.php

---
+++ trainers-chart.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model/trainers-chart-db.php");
+  
+$pageTitle= "Trainers Chart";
+include "view-header.php";
+$trainers = selectTrainers();
+include "view-trainers-chart/page.php";
+include "view-footer.php";
+?>
