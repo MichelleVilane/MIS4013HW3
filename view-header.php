f680c98ec16797ee203a889be9077a9d60142b1a
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-11-14 23:48:56 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -28,6 +28,9 @@
          <li class="nav-item">    
          <a class="nav-link" href="trainers-with-classes.php">Trainers with Classes</a>
         </li>
+        <li class="nav-item">    
+         <a class="nav-link" href="trainers-chart.php">Trainers Chart</a>
+        </li>
         
       </ul>
      
