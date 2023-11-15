18ad1fcb7186afab8733fcb9701beca9a1568095
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-11-15 00:12:36 -0600

    Update page.php

--- view/trainers-chart/page.php
+++ view/trainers-chart/page.php
@@ -1,84 +1,59 @@
 <h1>Trainers chart</h1>
+
 <div>
   <canvas id="myChart"></canvas>
 </div>
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script>
   const ctx = document.getElementById('myChart');
+  const trainersData = [];
+
+  <?php
+  $trainers = selectTrainers();
+  while ($trainer = $trainers->fetch_assoc()) {
+    echo "trainersData.push({name: '{$trainer['trainer_name']}', sessions: {$trainer['num_sessions']}});";
+  }
+  ?>
 
   new Chart(ctx, {
     type: 'doughnut',
     data: {
-    datasets: [{
-        data: [
-<?php
-while ($trainer=$trainers -> fetch_assoc()) {
-  echo $trainer['num_sessions'] . ", ";
-}
-?>     
-        ]
-    }],
-
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
-        <?php
-$trainers= selectTrainers();
-while ($trainer=$trainers -> fetch_assoc()) {
-  echo "'" . $trainer['trainer_name'] . "', ";
-}
-?> 
-    ]
-},
-    
+      datasets: [{
+        data: trainersData.map(trainer => trainer.sessions),
+      }],
+      labels: trainersData.map(trainer => trainer.name),
+    },
   });
-
-  
- 
 </script>
 
-
- <div id="chart"></div>
+<div id="chart"></div>
 <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-   <script>
-    var options = {
-          series: [{
-          data: {
-    datasets: [{
-        data: [
-<?php
-while ($trainer=$trainers -> fetch_assoc()) {
-  echo $trainer['num_sessions'] . ", ";
-}
-?>     
-        ]
+
+<script>
+  var options = {
+    series: [{
+      data: trainersData.map(trainer => trainer.sessions),
     }],
-          chart: {
-          type: 'bar',
-          height: 350
-        },
-        plotOptions: {
-          bar: {
-            borderRadius: 4,
-            horizontal: true,
-          }
-        },
-        dataLabels: {
-          enabled: false
-        },
-        xaxis: {
-          categories:  [
-        <?php
-$trainers= selectTrainers();
-while ($trainer=$trainers -> fetch_assoc()) {
-  echo "'" . $trainer['trainer_name'] . "', ";
-}
-?> 
-    ]
-},
-        var chart = new ApexCharts(document.querySelector("#chart"), options);
-        chart.render();
-       </script> 
- 
-   
+    chart: {
+      type: 'bar',
+      height: 350,
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        horizontal: true,
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    xaxis: {
+      categories: trainersData.map(trainer => trainer.name),
+    },
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  chart.render();
+</script>
