fd69371b368e3be4b41178c21663cf394bb65fdf
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-11-14 21:50:23 -0600

    Update page.php

--- view/trainers-chart/page.php
+++ view/trainers-chart/page.php
@@ -1,9 +1,38 @@
 <h1>Trainers chart</h1>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-        <?php
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+    datasets: [{
+        data: [
+<?php
 while ($trainer=$trainers -> fetch_assoc()) {
-  
+  echo $instructor['num_sessions'] . ", ";
 }
-?>
+?>     
+        ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        <?php
+$trainers= selectTraners();
+while ($trainer=$trainers -> fetch_assoc()) {
+  echo "'" . $trainer['trainer_name'] . "', ";
+}
+?> 
+    ]
+},
+    
+  });
+</script>
  
    
