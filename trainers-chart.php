fb1e1b3d4756ca336a8f6b6063a1b0d1b599f2fc
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-11-14 22:27:38 -0600

    Update trainers-chart.php

--- trainers-chart.php
+++ trainers-chart.php
@@ -5,6 +5,6 @@ require_once("model/trainers-chart-db.php");
 $pageTitle= "Trainers Chart";
 include "view-header.php";
 $trainers = selectTrainers();
-include "view-trainers-chart/page.php";
+include "view/trainers-chart/page.php";
 include "view-footer.php";
 ?>
