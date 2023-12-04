7f163a43dfac987202832c4ad0c0a528a86c0dd9
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-03 23:09:03 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -20,7 +20,7 @@
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="trainers.php">Scheduled Dates</a>
+          <a class="nav-link" href="trainers.php">Suitors</a>
         </li>
          <li class="nav-item">           
           <a class="nav-link" href="classes.php">Dates</a>
