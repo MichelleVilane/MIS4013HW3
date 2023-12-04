c5df6d88b9c27d6885ded7df9852802ee20b2b40
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-04 02:25:07 -0600

    Update page.php

--- view/trainers-with-classes/page.php
+++ view/trainers-with-classes/page.php
@@ -39,7 +39,7 @@ while ($trainer=$trainers -> fetch_assoc()) {
 <div class="row row-cols-1 row-cols-md-2 g-4">
   <div class="col">
     <div class="card">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="man.webp" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -57,7 +57,7 @@ while ($trainer=$trainers -> fetch_assoc()) {
   </div>
   <div class="col">
     <div class="card">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="leo.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
@@ -66,7 +66,7 @@ while ($trainer=$trainers -> fetch_assoc()) {
   </div>
   <div class="col">
     <div class="card">
-      <img src="..." class="card-img-top" alt="...">
+      <img src="mgb.webp" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
