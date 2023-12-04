abd785536d6220327ecd3ef86af121b0ff5082b7
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-03 18:22:40 -0600

    Update session-by-class.php

--- session-by-class.php
+++ session-by-class.php
@@ -5,6 +5,6 @@ require_once("model/session-by-class.php");
 $pageTitle= "Session By Class";
 include "view-header.php";
 $sessions = selectSessionByClass($_POST['cid']);
-include "view-session-by-class.php";
+include "view/session-by-class.php";
 include "view-footer.php";
 ?>
