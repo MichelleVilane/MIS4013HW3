19a8920be60872573958dd1182b0b406e97b78b0
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-04 02:22:21 -0600

    Update page.php

--- view/trainers-with-classes/page.php
+++ view/trainers-with-classes/page.php
@@ -26,6 +26,15 @@ while ($trainer=$trainers -> fetch_assoc()) {
 }
 ?>
 </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body>
 
 <div class="row row-cols-1 row-cols-md-2 g-4">
   <div class="col">
@@ -65,3 +74,5 @@ while ($trainer=$trainers -> fetch_assoc()) {
     </div>
   </div>
 </div>
+</body>
+</html>
