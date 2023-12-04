c78a64dd67e394cd2194022a15ba57389957a89e
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-03 23:06:53 -0600

    Update classes-by-trainer.php

--- classes-by-trainer.php
+++ classes-by-trainer.php
@@ -2,7 +2,7 @@
 require_once("util-db.php");
 require_once("model/classes-by-trainer.php");
   
-$pageTitle= "Classes by Trainer";
+$pageTitle= "Additional Information";
 include "view-header.php";
 $classes = selectClassesByTrainer($_GET['id']);
 include "view/classes-by-trainer.php";
