82e0406ceece8365957c34ef674dbdf40e0a458e
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-03 21:31:16 -0600

    Update index.php

--- index.php
+++ index.php
@@ -15,10 +15,11 @@ include "view-header.php";
     <div class="container">
    <div class="text-bg-secondary p-3"><h1>Welcome to OUr One Stop Shop</h1></div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-
- <img src="JEDN2954.jpeg" class="img-thumbnail" alt="...">
+<picture>
+  <source srcset="..." type="image/svg+xml">
+  <img src="JEDN2954.jpeg" class="img-fluid img-thumbnail" alt="...">
+</picture>
      
-
       </div>
   </body>
 </html>
