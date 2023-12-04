a6eea1753248a2e270c820d295f16ab6ff931860
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-03 21:47:22 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 $pageTitle= "Home";
 include "view-header.php";
 ?>
- <h1>Michelle's Art Corner</h1>
 <!doctype html>
 <html lang="en">
   <head>
@@ -13,12 +12,9 @@ include "view-header.php";
   </head>
   <body>
     <div class="container">
-   <div class="text-bg-secondary p-3"><h1>Welcome to OUr One Stop Shop</h1></div>
+   <div class="text-bg-secondary p-3"><h1>Cupid's Bow Online Matchmaking</h1></div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-<picture>
-  <source srcset="..." type="image/svg+xml">
-  <img src="JEDN2954.jpeg" class="img-fluid img-thumbnail" alt="...">
-</picture>
+
      
       </div>
   </body>
