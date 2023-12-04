0c002c39f960307aa8c5bc9a02a8adc32373f18f
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-04 00:19:11 -0600

    Update classes.php

--- classes.php
+++ classes.php
@@ -9,21 +9,21 @@ if (isset($_POST['actionType'])) {
  switch ($_POST ['actionType']) {
    case "Add":
     if (insertClass($_POST['cNumber'], $_POST['cDescription'])) {
-      echo '<div class="alert alert-success" role="alert"> Course Added. </div>';
+      echo '<div class="alert alert-success" role="alert"> New Date Added! </div>';
     } else {
       echo '<div class="alert alert-danger" role="alert"> Error. </div>';
     }
      break;
      case "Edit":
     if (updateClass($_POST['cNumber'], $_POST['cDescription'], $_POST['cid'])) {
-      echo '<div class="alert alert-success" role="alert"> Course Edited. </div>';
+      echo '<div class="alert alert-success" role="alert"> Date Edited. </div>';
     } else {
       echo '<div class="alert alert-danger" role="alert"> Error. </div>';
     }
      break;
    case "Delete":
     if (deleteClass($_POST['cid'])) {
-      echo '<div class="alert alert-success" role="alert"> Course Deleted. </div>';
+      echo '<div class="alert alert-success" role="alert"> Date Cancelled. </div>';
     } else {
       echo '<div class="alert alert-danger" role="alert"> Error. </div>';
     }
