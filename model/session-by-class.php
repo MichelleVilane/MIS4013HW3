9021ba068b1b8c23ea0a6b958219130d63eb91bb
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-04 01:32:09 -0600

    Update session-by-class.php

--- model/session-by-class.php
+++ model/session-by-class.php
@@ -2,7 +2,7 @@
 function selectSessionByClass($cid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT c.class_id, class_number, class_description, day, session_id, trainer_name FROM `class`c join session s on s.class_id=c.class_id join trainer t on t.class_id=c.class_id where s.class_id=?");
+        $stmt = $conn->prepare("SELECT c.class_id, class_number, class_description, day, session_id, trainer_name FROM `class`c join session s on s.class_id=c.class_id  where s.class_id=?");
         $stmt->bind_param("i", $cid);
         $stmt->execute();
         $result = $stmt->get_result();
