1e1c23edce8d46b759115161ef8d1c8f62e86999
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-04 02:35:23 -0600

    Update page.php

--- view/trainers-with-classes/page.php
+++ view/trainers-with-classes/page.php
@@ -41,8 +41,8 @@ while ($trainer=$trainers -> fetch_assoc()) {
     <div class="card">
       <img src="Matt Damon3.jpg" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title">Matt Damon</h5>
+        <p class="card-text"> 🏹 Archery, literature, and the art of connection! 📚 As an English professor, I find joy in the power of words and the precision of a well-aimed arrow. When not lost in the pages of a good book, you'll likely find me on the archery range, blending the beauty of two worlds. Seeking someone who appreciates the charm of literature, the thrill of hitting the bullseye, and the magic that happens when shared passions align. If you're ready for a chapter of adventure and a love story with a poetic twist, swipe right! 🎯📖 #ArcheryAndWordsmith #CupidsBowChronicles@MattDamon on instagram.</p>
       </div>
     </div>
   </div>
@@ -50,8 +50,8 @@ while ($trainer=$trainers -> fetch_assoc()) {
     <div class="card">
       <img src="man.webp" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title">Nick Young</h5>
+        <p class="card-text">🌿 Outdoor enthusiast with a passion for painting, a heart for dogs, and a quest for a lifelong adventure partner! 🎨🐾 Whether I'm chasing sunsets on a hiking trail, channeling my creativity onto a canvas, or sharing moments of joy with my furry companion, life's all about the simple pleasures. On the lookout for a kindred spirit who values nature, appreciates the strokes of a paintbrush, and understands the unconditional love a dog brings. Let's explore the canvas of life together – if you're ready for a palette of love and laughter, swipe right! 🌄🖌️ #NatureLover #DogEnthusiast #CupidsBowAdventures.</p>
       </div>
     </div>
   </div>
@@ -59,8 +59,8 @@ while ($trainer=$trainers -> fetch_assoc()) {
     <div class="card">
       <img src="leo.jpg" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+        <h5 class="card-title">Leonardo DiCaprio</h5>
+        <p class="card-text">🎣 Passionate about reeling in big catches and casting a line into life's adventures! 🎉 As the life of the party, I bring the energy wherever I go, making every moment memorable. Whether it's a spontaneous road trip, a night of laughter, or a quiet day by the water, I believe in living life to the fullest. Looking for a partner who shares a love for spontaneity, good vibes, and the thrill of the catch. If you're ready to dive into a sea of fun and build a connection that's as exciting as a tight line, let's make waves together! 🚗🌊 #PartyAnimal #AdventureSeeker #CupidsBowFishing.</p>
       </div>
     </div>
   </div>
@@ -68,8 +68,8 @@ while ($trainer=$trainers -> fetch_assoc()) {
     <div class="card">
       <img src="mgb.webp" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title">Michael B Jordan</h5>
+        <p class="card-text">🏋️‍♂️ Fitness enthusiast, bookworm, and finance analyst by day! 📈 Embracing the balance of an active lifestyle and the tranquility of a good read. Whether I'm breaking a sweat at the gym or diving into the pages of a compelling story, life's about constant growth and exploration. I'm all about trying new things and finding joy in every experience. Seeking a partner who values both the thrill of staying active and the quiet moments with a good book. If you're ready to embark on a journey of fitness, literature, and financial adventures, swipe right! 📚💪 #ActiveLifestyle #BookishExplorer #CupidsBowFinance.</p>
       </div>
     </div>
   </div>
