a4a68182b69655bf162cc31a22eecbd7c204b5e9
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-04 00:12:16 -0600

    Update suitors.php

--- view/suitors.php
+++ view/suitors.php
@@ -8,10 +8,15 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 <body>
+  <div style="width: 100%; text-align: center;">
+    <span style="display: inline-block; margin: 0 auto; border: 5px solid black; padding: 10px;">
          <br></br>
     <p>Hello, Michelle! Welcome back to your Cupid's Bow user portal. Here is a list of your potential dates and their preferred location to meet. 
     to see more information about your suitor's meeting preferences, click the 'additional information' hyperlink. To make or cancel a date, please click on the 'Date' section of the navigation bar at the top of the screen. Goodluck!</p>
         <br></br>
+    </span>
+  </div>
+  
 </body>
 </html>
 <div class="table-responsive">
