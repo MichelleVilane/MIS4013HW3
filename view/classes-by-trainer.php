3df68ad0ba097e924215b8c8898817cbaecdae5b
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-03 23:59:00 -0600

    Update classes-by-trainer.php

--- view/classes-by-trainer.php
+++ view/classes-by-trainer.php
@@ -24,27 +24,7 @@ while ($class= $classes -> fetch_assoc()) {
   <?php
 }
        ?>
-<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="1000">
-      <img src="Matt Damon3.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item" data-bs-interval="1000">
-      <img src="Matt damon1.jfif" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="Matt damon2.webp" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+
       </tbody>
   </table>
 </div>
