f47d52f2e286d4c42e4072171978c4ea9fa76db4
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-04 00:59:53 -0600

    Update classes.php

--- model/classes.php
+++ model/classes.php
@@ -2,7 +2,7 @@
 function selectClasses() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT class_id, class_number, class_description, FROM `class`");
+        $stmt = $conn->prepare("SELECT class_id, class_number, class_description FROM `class`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
