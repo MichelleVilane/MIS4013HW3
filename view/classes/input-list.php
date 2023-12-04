864809a15d9fd30c9812d4bf81cc7cfe837a4da7
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-04 11:27:11 -0600

    Update input-list.php

--- view/classes/input-list.php
+++ view/classes/input-list.php
@@ -1,8 +1,12 @@
 <select class="form-select" id="cNumber" name="cNumber>
 <?php
 while ($classItem =$classList -> fetch assoc()) {
+  $selText="";
+  if ($selectedClass==$ClassItem['class_id']) {
+    $selText = " selected";
+  }
   ?>
-  <option value="<?php echo $classItem['class_id']">Three</option>
+  <option value="<?php echo $classItem['class_id'];?>"><?php echo $classItem['class_number'];?></option>
 <?php
 }
 ?>  
