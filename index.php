6768691b1cc827f8fb652e9e34e379498cf6c808
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-03 22:07:32 -0600

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include "view-header.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   </head>
   <body>
-   <span style= "width: 50%"; display: inline-block; >
+   <span style= "width: 50%; display: inline-block;">
    <div class="text-bg-secondary p-3"><h1>Cupid's Bow Online Matchmaking</h1></div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 <div id="carouselExampleRide" class="carousel slide" data-bs-ride="true">
