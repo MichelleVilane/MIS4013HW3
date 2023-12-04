a3f63a6fc3aa96059d74b1a5597588e9d118bda9
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-03 21:54:23 -0600

    Update index.php

--- index.php
+++ index.php
@@ -17,13 +17,13 @@ include "view-header.php";
 <div id="carouselExampleRide" class="carousel slide" data-bs-ride="true">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="couple2.webp" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="couple4.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="couple3.jpg" class="d-block w-100" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
