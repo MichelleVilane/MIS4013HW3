862f0175cee2bcfe0338772eecc43cad8e86a746
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-03 23:18:56 -0600

    Update trainers.php

--- view/trainers.php
+++ view/trainers.php
@@ -1,8 +1,19 @@
 <h1>Suitors</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body>
 
     <p>Hello, Michelle! Welcome back to your Cupid's Bow user portal. Here is a list of your potential dates and their preferred location to meet. 
     to make or cancel a date with any suitor, click the 'additional information' hyperlink. Goodluck!</p>
         <br></br>
+</body>
+</html>
 <div class="table-responsive">
   <table class="table">
     <thead>
