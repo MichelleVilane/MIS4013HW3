d6d80c150b356323bc1d7a6b38406ecdf30ec36a
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-05 21:56:31 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -12,7 +12,7 @@
   <body>
  <div class= "container-fluid">
     <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-  <div class="container">
+  <div class="container-fluid">
     <a class="navbar-brand" href="#">Cupid's Bow</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
