fb2302db91513974a187af688c16457b08d09405
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-05 19:15:02 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,35 +3,31 @@ $pageTitle= "Home";
 include "view-header.php";
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Bootstrap demo</title>
-  <!-- Replace the link with the path to your compiled Sass code -->
-  <link href="‪C:\Users\vilan\Downloads\_bootswatch.scss" rel="stylesheet">
-
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 <body>
 
 <div class="text-bg-danger p-3">
   <h1>Cupid's Bow Archery & Matchmaking - User Portal</h1>
 </div>
-<br></br>
-<div style="width: 100%; text-align: center;">
-  <span style="display: inline-block; margin: 0 auto; border: 5px solid gray; padding: 10px;">
+    <br></br>
+  <div style="width: 100%; text-align: center;">
+    <span style="display: inline-block; margin: 0 auto; border: 5px solid gray; padding: 10px;">
 
-    <p>Welcome to Cupid's Bow, where passion for archery meets the thrill of love! Immerse yourself in the world of arrows and affection as you embark on a unique journey to find your perfect match. Cupid's Bow is not just a matchmaking platform; it's a community where archery enthusiasts come together to share their love for the sport and explore romantic connections. Picture yourself meeting like-minded individuals who share your passion for archery, enjoying the excitement of hitting bullseyes while forging meaningful connections. Our platform seamlessly blends the art of matchmaking with the precision of archery, offering a one-of-a-kind experience for those who believe in the power of love and the joy of a well-aimed shot. Join Cupid's Bow, where love and archery collide, and let the adventure begin!</p>
-  </span>
-</div>
-<br></br>
-<div style="width: 100%; text-align: center;">
+<p>Welcome to Cupid's Bow, where passion for archery meets the thrill of love! Immerse yourself in the world of arrows and affection as you embark on a unique journey to find your perfect match. Cupid's Bow is not just a matchmaking platform; it's a community where archery enthusiasts come together to share their love for the sport and explore romantic connections. Picture yourself meeting like-minded individuals who share your passion for archery, enjoying the excitement of hitting bullseyes while forging meaningful connections. Our platform seamlessly blends the art of matchmaking with the precision of archery, offering a one-of-a-kind experience for those who believe in the power of love and the joy of a well-aimed shot. Join Cupid's Bow, where love and archery collide, and let the adventure begin!</p>
+    </span>
+  </div>
+    <br></br>
+      <div style="width: 100%; text-align: center;">
   <span style="display: inline-block; ">
-    <!-- Include Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-
-    <!-- Carousel -->
     <div id="carouselExampleRide" class="carousel slide" data-bs-ride="true">
       <div class="carousel-inner">
         <div class="carousel-item active">
@@ -56,26 +52,84 @@ include "view-header.php";
   </span>
 </div>
 
-<!-- Footer -->
-<footer class="text-center text-white" style="background-color: #f1f1f1;">
+  <!-- Remove the container if you want to extend the Footer to full width. -->
+
+
+  <footer class="text-center text-white" style="background-color: #f1f1f1;">
+  <!-- Grid container -->
   <div class="container pt-4">
-    <!-- Social media links -->
+    <!-- Section: Social media -->
     <section class="mb-4">
-      <!-- Add your social media links here -->
+      <!-- Facebook -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-facebook-f"></i>
+                                        
+</a>
+
+      <!-- Twitter -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-twitter"></i
+      ></a>
+
+      <!-- Google -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-google"></i
+      ></a>
+
+      <!-- Instagram -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-instagram"></i
+      ></a>
+
+      <!-- Linkedin -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-linkedin"></i
+      ></a>
+      <!-- Github -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-github"></i
+      ></a>
     </section>
+    <!-- Section: Social media -->
   </div>
+  <!-- Grid container -->
 
   <!-- Copyright -->
   <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
     © 2020 Copyright:
     <a class="text-dark" href="https://mdbootstrap.com/">Cupid's Bow.com</a>
   </div>
+  <!-- Copyright -->
 </footer>
+  
+</div>
 <!-- End of .container -->
-
 </body>
 </html>
-
 
 
  <?php
