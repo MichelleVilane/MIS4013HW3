c4601c41c32f6a3ced46fcf79edaaaaac0c7e738
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-05 21:33:38 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -10,9 +10,9 @@
 
   </head>
   <body>
-    <div class ="container>
+ 
     <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-  <div class="container-fluid">
+  <div class="container">
     <a class="navbar-brand" href="#">Cupid's Bow</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -20,13 +20,11 @@
     <div class="collapse navbar-collapse" id="navbarColor01">
       <ul class="navbar-nav me-auto">
         <li class="nav-item">
-          <a class="nav-link active" href="">Home
+          <a class="nav-link active" href="index.php">Home
             <span class="visually-hidden">(current)</span>
           </a>
         </li>
-         <li class="nav-item">
-           <a class="nav-link" href="index.php">Home</a>
-        </li>
+        
         <li class="nav-item">
            <a class="nav-link" href="suitors.php">Suitors</a>
         </li>
