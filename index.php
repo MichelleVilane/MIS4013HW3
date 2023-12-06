cab3958fd0a9e5deaaa33eb8a261bc70ab872fca
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-05 19:25:56 -0600

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ include "view-header.php";
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Bootstrap demo</title>
   <link href="‪C:\Users\vilan\Downloads\bootstrap.css" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   <style>
     /* Add your compiled Sass code here */
    
