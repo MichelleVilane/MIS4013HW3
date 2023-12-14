203547e4cd6a826e02048bb9082053dd3be914e1
Michelle Vilane <111925113+MichelleVilane@users.noreply.github.com>
2023-12-14 14:23:25 -0600

    Update trainers-with-classes.php

--- trainers-with-classes.php
+++ trainers-with-classes.php
@@ -1,6 +1,6 @@
 <?php
 require_once("util-db.php");
-require_once("model/trainers-with-classes.php");
+require_once("model/suitor-profiles.php");
   
 $pageTitle= "Suitor Profile";
 include "view-header.php";
